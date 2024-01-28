e2910a21f7043718be87edc2db9fd1caf44667db
Mohammed Amine Khachchab <72837868+Med-Amine-Khachchab@users.noreply.github.com>
2024-01-28 22:40:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember(){
+    var skills = [
+        {name: "Java", level: "80%"},
+        {name: "Javascript", level: "70%"},
+        {name: "Python", level: "50%"},
+        {name: "HTML", level: "90%"},
+        {name: "CSS", level: "80%"},
+        {name: "PHP", level: "50%"},
+        {name: "C++", level: "40%"},
+        {name: "C", level: "30%"},
+        {name: "C#", level: "20%"},
+        {name: "SQL", level: "60%"},
+        {name: "Oracle", level: "60%"},
+        {name: "MongoDB", level: "50%"},
+        {name: "Node.js", level: "50%"},
+        {name: "React.js", level: "30%"},
+        {name: "Angular.js", level: "30%"}
+    ];
+}
